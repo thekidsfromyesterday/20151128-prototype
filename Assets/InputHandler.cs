52aad7863ff668679d154c1c8a6244cf8ff78fd2
Michael <michael.roberts.mail@gmail.com>
2015-11-28 16:10:23 +1100

    Updated animate in input handler

--- Assets/InputHandler.cs
+++ Assets/InputHandler.cs
@@ -8,29 +8,32 @@ public class InputHandler : MonoBehaviour
     private const string JumpInput = "Jump";
 
     public float movementSpeed = 5;
-    
+
     private Rigidbody2D RigidBody { get; set; }
+	private Animator Animator { get; set;}
 
     private bool IsGrounded
     {
-        get { return false; }
+        get { return RigidBody.velocity.y.Equals(0); }
     }
 
 	// Use this for initialization
 	void Start () {
         RigidBody = gameObject.GetComponent<Rigidbody2D>();
+		Animator = GetComponent<Animator> ();
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
-        Horizontal();
+		var axis = Input.GetAxis(HorizontalInput);
+        Horizontal(axis);
 	    Jump();
+		Animate(axis);
 	}
 
-    void Horizontal()
-    {
-        var axis = Input.GetAxis(HorizontalInput);
+    void Horizontal(float axis)
+    {        
         gameObject.transform.Translate(Vector3.right * axis * movementSpeed * Time.deltaTime);
     }
 
@@ -44,4 +47,24 @@ public class InputHandler : MonoBehaviour
     {
         Debug.Log(collision.gameObject.name);
     }
+
+	void Animate(float axis)
+	{
+		if (axis == 0) 
+		{		// Not moving
+			Animator.SetBool("isIdle", true);
+			Animator.SetBool("isMovingLeft", false);
+			Animator.SetBool("isMovingRight", false);
+		} else if (axis > 0)
+		{	// Moving right
+			Animator.SetBool("isIdle", false);
+			Animator.SetBool("isMovingLeft", false);
+			Animator.SetBool("isMovingRight", true);
+		} else if (axis < 0) 
+		{	// Moving left		
+			Animator.SetBool("isIdle", false);
+			Animator.SetBool("isMovingLeft", true);
+			Animator.SetBool("isMovingRight", false);
+		}
+	}
 }

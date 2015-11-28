8a5e57b6258c0d4a0ee389e3e792b16c29f82ba7
Michael <michael.roberts.mail@gmail.com>
2015-11-28 15:34:33 +1100

    Animation
    Added changes to the animator. Walk left - animate left etc.

--- Assets/InputScript.cs
+++ Assets/InputScript.cs
@@ -2,14 +2,37 @@
 using System.Collections;
 
 public class InputScript : MonoBehaviour {
-
+	Animator anim;
 	// Use this for initialization
 	void Start () {
-	
+		anim = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
+	//// Sets the value
+	//anim.SetBool("InConga", true); 
+	// Gets the value
+	//bool isInConga = anim.GetBool("InConga");
+
 	void Update () {
-	
+		var axis = Input.GetAxis ("Horizontal");
+		if (axis == 0) {		// Not moving
+			anim.SetBool("isIdle", true);
+			anim.SetBool("isMovingLeft", false);
+			anim.SetBool("isMovingRight", false);
+		} else if (axis > 0){	// Moving right
+			anim.SetBool("isIdle", false);
+			anim.SetBool("isMovingLeft", false);
+			anim.SetBool("isMovingRight", true);
+		} else if (axis < 0) {	// Moving left		
+			anim.SetBool("isIdle", false);
+			anim.SetBool("isMovingLeft", true);
+			anim.SetBool("isMovingRight", false);
+		}
+
+		var position = gameObject.transform.position;
+		var scalar = 0.1f;
+		gameObject.transform.position = new Vector3 (position.x + (axis * scalar), position.y, position.z);
+
 	}
 }
